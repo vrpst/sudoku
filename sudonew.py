4370c1f593a55b047af9cd17f17efe0bdc93ea5f
vrpst <lucas.verpoest@gmail.com>
2024-11-21 15:39:58 +0000

    fix elim functions and add hidden singles

--- sudonew.py
+++ sudonew.py
@@ -3,15 +3,15 @@ import copy
 import time  # for displaying
 
 sudoku_inp = [
-    [0, 2, 7, 8, 0, 0, 0, 0, 9],
-    [0, 0, 4, 0, 0, 0, 5, 3, 0],
-    [0, 6, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 5, 7, 4, 0, 0],
-    [0, 0, 0, 3, 0, 0, 0, 1, 0],
-    [9, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 8, 0, 0, 5, 0, 0, 2],
-    [0, 0, 0, 7, 0, 0, 0, 0, 5],
-    [0, 0, 1, 0, 2, 0, 0, 7, 0],
+    [0, 0, 0, 0, 6, 2, 3, 0, 0],
+    [3, 4, 9, 0, 1, 0, 7, 0, 0],
+    [0, 5, 0, 4, 3, 0, 0, 0, 1],
+    [0, 0, 2, 6, 5, 0, 0, 0, 9],
+    [0, 0, 8, 0, 4, 0, 1, 6, 2],
+    [0, 6, 4, 2, 9, 1, 0, 3, 8],
+    [0, 0, 0, 0, 0, 6, 0, 0, 0],
+    [0, 8, 0, 0, 7, 0, 0, 5, 4],
+    [9, 0, 0, 3, 2, 0, 6, 0, 7],
 ]
 
 class Solve:
@@ -20,6 +20,14 @@ class Solve:
         self.__matrix = copy.deepcopy(inp)
         self.__hypo_matrix = self.__makeHypoMatrix()
         self.__updateHypoMatrix()
+        self.matrixPrint(self.__hypo_matrix)
+
+    def run(self):
+        self.__loneSingles()
+        self.__hiddenSingles()
+        self.matrixPrint(self.__hypo_matrix)
+        print("\n")
+        self.matrixPrint(self.__matrix)
 
     def matrixPrint(self, inp):
         for i in inp:
@@ -34,17 +42,40 @@ class Solve:
         return hypomat
 
     def __openSingles(self):  # all boxes filled but that one
-        pass
+        pass  # shouldn't be needed because of lone singels
 
-    def __loneSingles(self):  # only one number that can go in the box 
+    def __loneSingles(self):  # only one number that can go in the cell 
         for r in range(9):
             for c in range(9):
-                if len(self.__hypo_matrix[r][c]) == 0:
+                if len(self.__hypo_matrix[r][c]) == 1:
+                    print("ls", r, c)
                     self.__matrix[r][c] = self.__hypo_matrix[r][c][0]
                     self.__updateHypoMatrix()
 
     def __hiddenSingles(self):  # only box a specific number can go in, but multiple numbers can go in the box
-        pass
+        # row
+        for r in range(9):
+            for n in range(9):
+                n_in = []
+                for c1 in range(9):
+                    if n+1 in self.__hypo_matrix[r][c1]:
+                        n_in.append(c1)
+                if len(n_in) == 1:
+                    self.__matrix[r][n_in[0]] = n+1
+                    print("HIDDEN ROW")
+                    self.__updateHypoMatrix()
+        
+        # column
+        for c in range(9):
+            for n in range(9):
+                n_in = []
+                for r1 in range(9):
+                    if n+1 in self.__hypo_matrix[r1][c]:
+                        n_in.append(r1)
+                if len(n_in) == 1:
+                    self.__matrix[n_in[0]][c] = n+1
+                    print("HIDDEN COL")
+                    self.__updateHypoMatrix()
 
     def __nakedPairs(self):  # two boxes with only one and the same pair of numbers that can go in
         pass
@@ -63,40 +94,35 @@ class Solve:
         for r in range(9):
             for c in range(9):
                 if self.__matrix[r][c] != 0:
-                    print(self.__hypo_matrix[r][c])
                     self.__hypo_matrix[r][c] = []
 
     def __updateHypoMatrixImplicit(self):  # remove from a box when number placed near
         # row
         for r in range(9):
             for c1 in range(9):
-                for n in range(9):
-                    if n+1 == self.__matrix[r][c1]:
-                        for c2 in range(9):
-                            if n+1 in self.__hypo_matrix[r][c2]:
-                                print(r, n)
-                                self.__hypo_matrix[r][c2].remove(n+1)
+                if self.__matrix[r][c1] != 0:
+                    for c2 in range(9):
+                        if self.__matrix[r][c1] in self.__hypo_matrix[r][c2]:
+                            self.__hypo_matrix[r][c2].remove(self.__matrix[r][c1])
+    
         # column
         for c in range(9):
             for r1 in range(9):
-                for n in range(9):
-                    if n+1 == self.__hypo_matrix[r1][c]:
-                        for r2 in range(9):
-                            if n+1 in self.__hypo_matrix[r2][c]:
-                                self.__hypo_matrix[r2][c].remove(n+1)
+                if self.__matrix[r1][c] != 0:
+                    for r2 in range(9):
+                        if self.__matrix[r1][c] in self.__hypo_matrix[r2][c]:
+                            self.__hypo_matrix[r2][c].remove(self.__matrix[r1][c])
 
         # box
         for bx in range(3):
             for by in range(3):
                 for bx2 in range(3):
                     for by2 in range(3):
-                        for n in range(9):
-                            if n+1 == self.__matrix[bx*3+bx2][by*3+by2]:
-                                for bx3 in range(3):
-                                    for by3 in range(3):
-                                        if n+1 in self.__hypo_matrix[bx*3+bx3][by*3+by3]:
-                                            self.__hypo_matrix[bx*3+bx3][by*3+by3].remove(n+1)
-        
-        self.matrixPrint(self.__hypo_matrix)
-
-Solve(sudoku_inp)
+                        if self.__matrix[bx*3+bx2][by*3+by2] != 0:
+                            for bx3 in range(3):
+                                for by3 in range(3):
+                                    if self.__matrix[bx*3+bx2][by*3+by2] in self.__hypo_matrix[bx*3+bx3][by*3+by3]:
+                                        self.__hypo_matrix[bx*3+bx3][by*3+by3].remove(self.__matrix[bx*3+bx2][by*3+by2])
+    
+sd = Solve(sudoku_inp)
+sd.run()
